e69effe3e949613561ac35edb1f489c171f098d0
Ravisk02 <kumbarravi964@gmail.com>
2025-11-29 12:07:35 +0530

    Final Project code

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState,useEffect } from 'react';
+import Taskform from './Components/Taskform'
+import Tasklist from './Components/TaskList'
+import Progresstracker from './Components/ProgressTracker'
+import './Style.css'
+
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+  useEffect(()=>{
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+  });
+
+  const addTask = (task)=>{
+    setTasks([...tasks, task])
+  }
+  const updateTask = (updatedTask, index) => {
+    const newtask = [...tasks];
+    newtask[index] = updatedTask;
+    setTasks(newtask);
+  }
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i != index ))
+  }
+  const clearTasks = () => {
+    setTasks([]);
+  }
+  return (
+    <div>
+      <header>
+        <h1>TaskBuddy</h1>
+        <p><i>Your friendly Task Alertsys</i></p>
+      </header>
+      <Taskform addTask = {addTask}/>
+      <Tasklist tasks = {tasks}
+       updateTask = {updateTask} 
+       deleteTask = {deleteTask}/>
+      <Progresstracker tasks = {tasks}/>
+      {tasks.length>0 && (<button className='clear-btn'
+      onClick={clearTasks}>clear All Tasks</button>)}
+    </div>
+  )
+}
